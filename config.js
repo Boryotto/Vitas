0e9c8110186a16ecc522a7472f8603c7c635cf07
mor747111 <mor.k.747111@gmail.com>
2017-03-06 19:09:26 +0200

    Added configuration support

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+    generatedIdLength: 10
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const idLength = 10;
+const idLength = config.generatedIdLength;
 const categoryRegistryStorageKey = 'categories';
 
 let categoryStorer = new CategoryStorer(categoryRegistryStorageKey);
@@ -11,3 +11,12 @@ let vitas = new Vitas(categoryStorer, downloadListener, idGenerator, popupAgent)
 vitas.start();
 
 popupAgent.display();
+
+let messageRouter = new MessageRouter();
+let route = new MessageRoute('GET', 'downloads/4', () => {
+    console.log("callback called!!!");
+});
+
+messageRouter.addMessageRoute(route);
+
+messageRouter.removeMessageRoute(route.messageType, route.hook);
